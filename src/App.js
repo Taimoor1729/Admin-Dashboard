ae8dff57cf842ae7a8fa0eddc22127adc979b575
Taimoor1729 <temitech669@gmail.com>
2025-04-22 16:36:25 +0500

    first step in base setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import New from "./pages/new/New";
+import Login from "./pages/login/Login";
+import Single from "./pages/single/Single";
+import List from "./pages/list/List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users" element={<List />} />
+        <Route path="/users/:userId" element={<Single />} />
+        <Route path="/users/new" element={<New />} />
+        <Route path="/product" element={<List />} />
+        <Route path="/product/:productId" element={<Single />} />
+        <Route path="/product/new" element={<New />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import './navbar.scss'
+function Navbar() {
+    return (
+      <div className="App">
+     lama
+      </div>
+    );
+  }
+  
+  export default Navbar;
