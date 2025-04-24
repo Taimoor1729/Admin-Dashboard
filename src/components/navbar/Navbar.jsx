747fe2674f270f733bf7a3af2d028ef42e070bbf
Taimoor1729 <temitech669@gmail.com>
2025-04-24 15:11:28 +0500

    navbar initial step

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,53 @@
+import { ChatBubbleOutlineOutlined, DarkModeOutlined, FullscreenExitOutlined, LanguageOutlined, ListOutlined, NotificationsNoneOutlined, SearchOutlined } from '@mui/icons-material';
 import './navbar.scss'
+
 function Navbar() {
-    return (
-      <div className="App">
-     lama
+  return (
+    <div className="navbar">
+      <div className='wrapper'>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <SearchOutlined className='icon' />
+
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <LanguageOutlined className='icon' />
+            english
+          </div>
+          <div className='item'>
+            <DarkModeOutlined className='icon' />
+
+          </div>
+          <div className='item'>
+            <FullscreenExitOutlined className='icon' />
+
+          </div>
+          <div className='item'>
+            <NotificationsNoneOutlined className='icon' />
+            <div className='counter'>1</div>
+
+          </div>
+          <div className='item'>
+            <ChatBubbleOutlineOutlined className='icon' />
+            <div className='counter'>2</div>
+          </div>
+          <div className='item'>
+            <ListOutlined className='icon' />
+
+          </div>
+          <div className='item'>
+            <img
+              src='https://unsplash.com/photos/a-small-toy-is-standing-in-front-of-a-pink-background-jlJpDBK17Hw'
+              alt=''
+              className='avatar'
+            />
+          </div>
+        </div>
+
       </div>
-    );
-  }
-  
-  export default Navbar;
+    </div>
+  );
+}
+
+export default Navbar;
